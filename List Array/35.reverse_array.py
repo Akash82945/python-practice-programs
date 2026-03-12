53fe71dd9d19f887189f36cc06bbfd2a4fe1613e
Akash82945 <akashkumar9576386319@gmail.com>
2026-03-13 00:25:18 +0530

    Reverse the array.

---
+++ List Array/35.reverse_array.py
@@ -0,0 +1,18 @@
+n = int(input("Enter the length of array: "))
+
+array = []
+
+for i in range (n):
+    i = int(input("Enter the values: "))
+    array.append(i)
+    
+print(array)
+
+if len(array) == 0:
+    print("The array is empty.")
+else:
+    reverse_array = []
+    for x in array[::-1]:
+        reverse_array.append(x)
+        
+print(f"This is the reverserd array {reverse_array}")
