c1ecbd7db16b48da92fcb5ced673281ea1fcffa1
Akash82945 <akashkumar9576386319@gmail.com>
2026-03-19 23:08:00 +0530

    Vowels Check

---
+++ Function/58.count_vowels.py
@@ -0,0 +1,12 @@
+def count_vowels(string):
+    
+    vowels = "aeiouAEIOU"
+    
+    count = 0
+    
+    for i in string:
+        if i in vowels:
+            count += 1
+    return count
+        
+print(count_vowels("Akash").lower())
