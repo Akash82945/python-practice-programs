7204261793d1b3895307f470bd4b5ee50b7b0a26
Akash82945 <akashkumar9576386319@gmail.com>
2026-04-18 23:44:50 +0530

    Updated code.

--- 09 Opps/90.simple_calculater_class.py
+++ 09 Opps/90.simple_calculater_class.py
@@ -1,8 +1,12 @@
 class calculater():
     
-    def __init__(self,x,y):
-        self.x = x 
-        self.y = y
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        
+    def get_input(self):
+        self.x = int(input("Enter Value of x : "))
+        self.y = int(input("Enter Value of y : "))
         
     def addition(self):
         sum = self.x + self.y
@@ -17,14 +21,18 @@ class calculater():
         return mul
     
     def division(self):
-        div = self.x / self.y
-        return div
+        while True:
+            if self.y == 0:
+                print("Error! Zero Division Not Possible. Try Again")
+            else:
+                div = self.x / self.y
+            return div
     
     def square(self):
         user_input = int(input("Enter number for finding square : "))
-        self.x = user_input
-        square = self.x * self.x
+        square = user_input * user_input
         return square
+    
     
     def result(self):
         print("=== Simple Calculater ===")
@@ -34,22 +42,33 @@ class calculater():
               2. Subtraction
               3. Multiplication
               4. Devision
-              5. Check Square''')
-        choise = int(input("Choose any One [1/2/3/4/5] : "))
-        if choise == 1:
-            return f"\nAddition : {self.addition()}"
-        elif choise == 2:
-            return f"\nSubtraction : {self.subtraction()}"
-        elif choise == 3:
-            return f"\nMultiplication : {self.multiplication()}"
-        elif choise == 4:
-            return f"\nDivision : {self.division()}" 
-        elif choise == 5:
-            return f"\nSquare : {self.square()}"
-        else:
-            return "Invalid Choise"
+              5. Check Square
+              6. Clear
+              7. Exit''')
         
+        while True:
+            choise = int(input("Choose any One [1/2/3/4/5/6/7] : "))
+            if choise == 1:
+                print( f"\nAddition : {self.addition()}")
+            elif choise == 2:
+                print( f"\nSubtraction : {self.subtraction()}")
+            elif choise == 3:
+                print( f"\nMultiplication : {self.multiplication()}")
+            elif choise == 4:
+                print( f"\nDivision : {self.division()}") 
+            elif choise == 5:
+                print( f"\nSquare : {self.square()}")
+            elif choise == 6:
+                self.x ,self.y = 0 , 0
+                print("Result Clear.")
+            elif choise == 7:
+                break
+            else:
+                print("Invalid Choise")
+        
+            
     
     
-num1 = calculater(5,5)
+num1 = calculater()
+print(num1.get_input())
 print(num1.result())
