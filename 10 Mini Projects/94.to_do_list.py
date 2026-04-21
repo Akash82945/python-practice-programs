6aee49019f6f5e7d4a6796be0b6f2efe1222072b
Akash82945 <akashkumar9576386319@gmail.com>
2026-04-21 22:59:06 +0530

    Change some feature.

--- 10 Mini Projects/94.to_do_list.py
+++ 10 Mini Projects/94.to_do_list.py
@@ -1,44 +1,62 @@
 def to_do_list():
-    print(" ===== TO DO LIST =====")
+    print(" \n===== TO DO LIST =====")
     
     
     task = []
     
     while True:
-        print("Choose what you want!")
+        
+        print("\nChoose what you want!")
         print('''
             1. Show Task
-            2. Mark Task Done
-            3. Add Task
+            2. Add Task
+            3. Mark Task Done
             4. Delete Task
             5. Exit
             ''')
-        choise = int(input("Enter Your Choise: "))
+        
+        try :
+            choise = int(input("Enter Your Choise: "))
+        except ValueError:
+            print("Please Enter a number (1-5)")
+            continue
+        
         if choise == 1:
             if not task:
                 print("Empty Task.")
-            for i in task:
-                print(f"{i} {task}")
-            print(f"Your Tasks.\n{task}")
+            for i,t in enumerate(task, 1): 
+                status = '✔' if t['done'] else " "
+                print(f"{i } [{status}] {t['Task']}")
+
         
         elif choise == 2:
             input_task = input("Enter Your Task : ")
-            task.append(input_task)
+            task.append({"Task": input_task , "done":False})
             print("Task Added Successfully.")
             
         elif choise == 3:
-            mark_done = int(input("Enter 1 for mark Done : "))
-            task[mark_done]['Done'] = True
-            print('Great Job!')
+            if not task:
+                print("Nothig to Mark as done.")
+                continue
+            idx = int(input("Enter Task no to mark Done: ")) -1
+            if 0 <= idx < len(task):
+                task[idx]['done'] = True
+                print("Great Job!")
+            else:
+                print("Invalid Task NO.")
             
         
         elif choise == 4:
-            remove_task = int(input("Enter Task no Which you Can delete: "))
-            if remove_task in task:
-                task.remove(remove_task)
-                print("Task Removed Successfully.")
+            if not task:
+                print("Nothing to Delete")
+                continue
+            
+            idx = int(input("Enter Task no to delete: ")) -1
+            if 0 <= idx < len(task):
+                removed = task.pop(idx)
+                print(f"Removed : {removed['input_task']}")
             else:
-                print("Task is not present in List.")
+                print("Invalid task NO. ")
                 
         elif choise == 5:
             print("Good Bye!")
@@ -46,7 +64,6 @@ def to_do_list():
         
         else:
             print("Invalid Choise.")
-            
             
             
 to_do_list()
