bba2ba298f761b7aed313d7aeb74a5cea92b0f71
Akash82945 <akashkumar9576386319@gmail.com>
2026-04-22 22:39:56 +0530

    Mini Project Random Quotes Generate.

--- 10 Mini Projects/95.random_quote_generator.py
+++ 10 Mini Projects/95.random_quote_generator.py
@@ -1,21 +1,20 @@
 import random
 
-quotes_dict = {
-    "Steve Jobs": "The only way to do great work is to love what you do.",
-    "Albert Einstein": "In the middle of every difficulty lies opportunity.",
-    "Winston Churchill": "Success is not final, failure is not fatal: it is the courage to continue that counts.",
-    "Ralph Waldo Emerson": "Do not go where the path may lead, go instead where there is no path and leave a trail.",
-    "John Lennon": "Life is what happens when you're busy making other plans.",
-    "George Addair": "Everything you've ever wanted is on the other side of fear.",
-    "Henry Ford": "Whether you think you can or you think you can't, you're right.",
-    "Chinese Proverb": "The best time to plant a tree was 20 years ago. The second best time is now.",
-    "Abraham Lincoln": "Whatever you are, be a good one.",
-    "Franklin D. Roosevelt": "The only thing we have to fear is fear itself."
-}
+quotes = [
+    {"author": "Steve Jobs", "text": "The only way to do great work is to love what you do."},
+    {"author": "Albert Einstein", "text": "In the middle of every difficulty lies opportunity."},
+    {"author": "Winston Churchill", "text": "Success is not final, failure is not fatal: it is the courage to continue that counts."},
+    {"author": "Ralph Waldo Emerson", "text": "Do not go where the path may lead, go instead where there is no path and leave a trail."},
+    {"author": "John Lennon", "text": "Life is what happens when you're busy making other plans."},
+    {"author": "George Addair", "text": "Everything you've ever wanted is on the other side of fear."},
+    {"author": "Henry Ford", "text": "Whether you think you can or you think you can't, you're right."},
+    {"author": "Chinese Proverb", "text": "The best time to plant a tree was 20 years ago. The second best time is now."},
+    {"author": "Abraham Lincoln", "text": "Whatever you are, be a good one."},
+    {"author": "Franklin D. Roosevelt", "text": "The only thing we have to fear is fear itself."}
+]
 
 # Example: How to access a specific quote
-print(quotes_dict["Steve Jobs"])
+# print(quotes_dict["Steve Jobs"])
 
-# random_quote = random.choice(quotes_dict)
-
-# print(f"Random Quote : {['Text']} , Author : {['Author']}")
+random_quote = random.choice(quotes)
+print(f'\nToday Inspiration :\nAuthor Namr: "{random_quote["author"]}\nQuotes : "{random_quote["text"]}\n')
