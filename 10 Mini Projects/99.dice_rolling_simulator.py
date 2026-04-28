72c575b75488e30e3a3105e89ea37d91511c7559
Akash82945 <akashkumar9576386319@gmail.com>
2026-04-28 23:32:05 +0530

    Mini Project Dice Rolling Simulater.

--- 10 Mini Projects/99.dice_rolling_simulator.py
+++ 10 Mini Projects/99.dice_rolling_simulator.py
@@ -2,60 +2,69 @@ import random
 
 def dice_rolling():
     
-    dice = random.randint(1,6)
-    
-    if dice == 1:
-        print(f'''
-              +-----+
-              |     |
-              |  1  |
-              |     |
-              +-----+
-              ''')
-    
-    elif dice == 2:
-        print(f'''
-              +-----+
-              |     |
-              |  2  |
-              |     |
-              +-----+
-              ''')
+    while True:
         
-    elif dice == 3:
-        print(f'''
-              +-----+
-              |     |
-              |  3  |
-              |     |
-              +-----+
-              ''')
+        rolling = input("Press 'enter' to Rollin and 'q' for quit :" ).lower()
         
-    elif dice == 4:
-        print(f'''
-              +-----+
-              |     |
-              |  4  |
-              |     |
-              +-----+
-              ''')
-        
-    elif dice == 5:
-        print(f'''
-              +-----+
-              |     |
-              |  5  |
-              |     |
-              +-----+
-              ''')
-        
-    elif dice == 6:
-        print(f'''
-              +-----+
-              |     |
-              |  6  |
-              |     |
-              +-----+
-              ''')
+        dice = random.randint(1,6)
+            
+        if dice == 1:
+            print(f'''
+                    +-----+
+                    |     |
+                    |  1  |
+                    |     |
+                    +-----+
+                    ''')
+            
+        elif dice == 2:
+            print(f'''
+                    +-----+
+                    |     |
+                    |  2  |
+                    |     |
+                    +-----+
+                    ''')
+                
+        elif dice == 3:
+            print(f'''
+                    +-----+
+                    |     |
+                    |  3  |
+                    |     |
+                    +-----+
+                    ''')
+                
+        elif dice == 4:
+            print(f'''
+                    +-----+
+                    |     |
+                    |  4  |
+                    |     |
+                    +-----+
+                    ''')
+                
+        elif dice == 5:
+            print(f'''
+                    +-----+
+                    |     |
+                    |  5  |
+                    |     |
+                    +-----+
+                    ''')
+                
+        elif dice == 6:
+            print(f'''
+                    +-----+
+                    |     |
+                    |  6  |
+                    |     |
+                    +-----+
+                    ''') 
+                
+            
+        if rolling == 'Q'.lower():
+            print("Good Bye!")
+            break
     
 dice_rolling()
