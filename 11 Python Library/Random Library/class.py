8d21ea3611d73a90dcab1a44a2a068ae0dbd15ee
Akash82945 <akashkumar9576386319@gmail.com>
2026-05-11 22:44:29 +0530

    practice of random library.

--- 11 Python Library/Random Library/class.py
+++ 11 Python Library/Random Library/class.py
@@ -8,6 +8,15 @@ import random
 
 
 # choose random values
-colors = ['blue','red','orange','green']
-ran_cho = random.choice(colors)
-print(ran_cho)
+# colors = ['blue','red','orange','green']
+# ran_cho = random.choice(colors)
+# print(ran_cho)
+
+
+# shuffle list
+# number = [1,2,3,4,5,6,7,8,9]
+# shu = random.shuffle(number)
+# print(number)
+
+dice = random.randint(1,6)
+print(f"Dice number : {dice}")
