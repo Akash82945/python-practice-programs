2733eae131c4b052bbd6af56914dfb1d45e68800
Akash82945 <akashkumar9576386319@gmail.com>
2026-05-11 22:37:32 +0530

    Practice Python Library.

---
+++ 11 Python Library/Random Library/class.py
@@ -0,0 +1,13 @@
+# Class Practice
+
+import random
+
+# inport random integer
+# ran_int = random.randint(1,10)
+# print(ran_int)
+
+
+# choose random values
+colors = ['blue','red','orange','green']
+ran_cho = random.choice(colors)
+print(ran_cho)
