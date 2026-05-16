2b42bd49c54b61a2f1618b98c9257606a2bb2c56
Akash82945 <akashkumar9576386319@gmail.com>
2026-05-16 23:00:02 +0530

    Print current time using datetime library

--- 11 Python Library/Datetime Library/datetime_class.py
+++ 11 Python Library/Datetime Library/datetime_class.py
@@ -1,5 +1,20 @@
+# Current date and time
+# import datetime
+# now = datetime.datetime.now()
+# print(now)
+
+
+# Current date
+# import datetime
+# today = datetime.date.today()
+# print(today)
+
+
+# Current time
 import datetime
-
-now = datetime.datetime.now()
-
-print(now)
+# current_time = datetime.datetime.now().time()
+# current_time = datetime.datetime.now().time().replace(microsecond=0)
+# print(current_time)
+current_time = datetime.datetime.now()
+formate_time = current_time.strftime("%H:%M:%S")
+print(formate_time)
