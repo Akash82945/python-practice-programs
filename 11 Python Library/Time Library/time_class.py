ef57b60df302ec7c7188c334fab041deeb2b26af
Akash82945 <akashkumar9576386319@gmail.com>
2026-05-20 01:31:47 +0530

    Convert string time into real format time.

--- 11 Python Library/Time Library/time_class.py
+++ 11 Python Library/Time Library/time_class.py
@@ -51,59 +51,90 @@ from playsound import playsound  #type: ignore
 # import sys
 # from playsound import playsound  #type: ignore
 
-def get_time():
-    try:
-        while True:
-            try:
-                user_input = input("Enter Time format -> HH:MM:SS or MM:SS or SS : ")
+# def get_time():
+#     try:
+#         while True:
+#             try:
+#                 user_input = input("Enter Time format -> HH:MM:SS or MM:SS or SS : ")
                 
-                parts = list(map(int, user_input.split(":")))
+#                 parts = list(map(int, user_input.split(":")))
                 
-                if len(parts) == 3:
-                    h,m,s = parts
-                elif len(parts) == 2:
-                    h,m,s = 0, parts[0],parts[1]
-                elif len(parts) == 1:
-                    h,m,s = 0,0,parts[0]
-                else:
-                    print("Invalid time format.")
-                    continue
-                if m >= 60 or s >= 60 or h < 0 or m < 0 or s < 0:
-                    print("Also invlid time.")
-                    continue
-                return (h*3600)+(m*60)+s
+#                 if len(parts) == 3:
+#                     h,m,s = parts
+#                 elif len(parts) == 2:
+#                     h,m,s = 0, parts[0],parts[1]
+#                 elif len(parts) == 1:
+#                     h,m,s = 0,0,parts[0]
+#                 else:
+#                     print("Invalid time format.")
+#                     continue
+#                 if m >= 60 or s >= 60 or h < 0 or m < 0 or s < 0:
+#                     print("Also invlid time.")
+#                     continue
+#                 return (h*3600)+(m*60)+s
         
-            except ValueError:
-                print("Invalid Inputs")
+#             except ValueError:
+#                 print("Invalid Inputs")
     
-    except KeyboardInterrupt:
-        print("Stopped by user.")
-        sys.exit()
+#     except KeyboardInterrupt:
+#         print("Stopped by user.")
+#         sys.exit()
         
-total = get_time()
-os.system('cls' if os.name == 'nt' else 'clear')
+# total = get_time()
+# os.system('cls' if os.name == 'nt' else 'clear')
 
-print(" ===== Countdown Timer =====") 
-min,sec = divmod(total, 60)
-hrs,min = divmod(min,60)
-time_set = f"{hrs:02d} : {min:02d} : {sec:02d}"
+# print(" ===== Countdown Timer =====") 
+# min,sec = divmod(total, 60)
+# hrs,min = divmod(min,60)
+# time_set = f"{hrs:02d} : {min:02d} : {sec:02d}"
 
-print("-"*30)
-print(f"You Set Time ({time_set})")
-print("-"*30)
+# print("-"*30)
+# print(f"You Set Time ({time_set})")
+# print("-"*30)
 
-try:
-    while total >= 0:
-        min,sec = divmod(total, 60)
-        hrs,min = divmod(min,60)
-        time_set = f"{hrs:02d} : {min:02d} : {sec:02d}"
-        print('Time left : ',time_set, " ")
-        total -= 1
-        time.sleep(1)
+# try:
+#     while total >= 0:
+#         min,sec = divmod(total, 60)
+#         hrs,min = divmod(min,60)
+#         time_set = f"{hrs:02d} : {min:02d} : {sec:02d}"
+#         print('Time left : ',time_set, " ")
+#         total -= 1
+#         time.sleep(1)
 
-    print("Time's Up!")
-    print("Timer alert....")
-    playsound("faaah.mp3")
+#     print("Time's Up!")
+#     print("Timer alert....")
+#     playsound("faaah.mp3")
     
-except KeyboardInterrupt:
-    print("Timer Stopped by user.")
+# except KeyboardInterrupt:
+#     print("Timer Stopped by user.")
+
+
+
+
+
+# Time Zone converter
+# local_time = time.strftime("%H:%M:%S")
+# date = time.strftime("%d-%b-%Y")
+# global_time = time.gmtime()
+# live_time = time.localtime(time.time())
+# print(time.strftime("%d-%m-%Y %H:%M:%S" , live_time))
+
+# print(f"PC local Time : {local_time}")
+# print(f"Date : {date}")
+# # print(f"Global Time : {global_time}")
+# for gt in global_time:
+#     print(gt)
+
+
+
+
+# Advance: Convert string time to real time
+# string_time = input("Emter Time format -> (HH:MM:SS) : ")
+string_time = "25-12-2026 18:30:00"
+real_time1 = "%d-%m-%Y %H:%M:%S"
+real_time = time.strptime(string_time,real_time1)
+# print(real_time)
+format_time = time.strftime("%H:%M:%S",real_time)
+format_date = time.strftime("%d-%m-%Y",real_time)
+print(format_time)
+print(format_date)
