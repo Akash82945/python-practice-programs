2c0d24ca8f7a4b618469ec3219e534fa31755c96
Akash82945 <akashkumar9576386319@gmail.com>
2026-05-19 23:20:47 +0530

    Add some extra features.

--- 11 Python Library/Datetime Library/5.weekday_finder.py
+++ 11 Python Library/Datetime Library/5.weekday_finder.py
@@ -6,3 +6,20 @@ weekday = time.strftime("%a")
 full_weekday = time.strftime("%A")
 print("Short name of week day :",weekday)
 print('Full name of week day : ',full_weekday)
+
+
+weekday = time.weekday()          # weekday() start 0 from monday
+weekday2 = time.isoweekday()      # isoweekday() start 1 from monday
+print(weekday)
+print(weekday2)
+
+
+# isocalander is use for finding year , week , day
+# calendar = time.isocalendar()
+# print(calendar)
+
+
+if time.weekday() >= 5:
+    print("Yay! Its weekend holiday.")
+else:
+    print("Sorry!, Today weekday.")
