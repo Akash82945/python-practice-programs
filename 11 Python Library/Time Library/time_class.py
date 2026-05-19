d4ad67b14743ab41b30242e55d796789b35b6a6a
Akash82945 <akashkumar9576386319@gmail.com>
2026-05-19 23:58:30 +0530

    Practice some extra questions.

---
+++ 11 Python Library/Time Library/time_class.py
@@ -0,0 +1,41 @@
+import time
+import os
+
+
+# Delay program
+# print('Start')
+# time.sleep(3)
+# print('End')
+
+
+
+# Timer
+# start = time.time()
+# count = 0
+# for i in range(1,1000):
+#     count += 1
+# print("Python")
+# end = time.time()
+# print(end - start)
+
+
+# Count time for user input
+# start = time.time()
+# user_input = input("Enter your name : ")
+# end = time.time()
+# print('Your name : ',user_input)
+# print("Time taken : ", end - start)
+
+
+
+# Digital clock
+os.system('cls' if os.name == 'nt' else 'clear')
+try :
+    while True:
+        print("\033[H", end="")
+        print(" ===== Digital Clock =====\n")
+        digital_time = time.strftime("%H:%M:%S")
+        print("Time :  ",digital_time)
+        second = time.sleep(1)
+except KeyboardInterrupt:
+    print("\nClock off.")
