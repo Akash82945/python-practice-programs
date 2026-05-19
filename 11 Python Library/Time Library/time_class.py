164fd856c5a0d9bfc796829f4ac30a84296fff7d
Akash82945 <akashkumar9576386319@gmail.com>
2026-05-20 00:23:06 +0530

    Create timer reverse counting.

--- 11 Python Library/Time Library/time_class.py
+++ 11 Python Library/Time Library/time_class.py
@@ -29,13 +29,38 @@ import os
 
 
 # Digital clock
+# os.system('cls' if os.name == 'nt' else 'clear')
+# try :
+#     while True:
+#         print("\033[H", end="")
+#         print(" ===== Digital Clock =====\n")
+#         digital_time = time.strftime("%H:%M:%S")
+#         print("Time :  ",digital_time)
+#         second = time.sleep(1)
+# except KeyboardInterrupt:
+#     print("\nClock off.")
+
+
+
+
+# Reverse Timer
+
+input_hour = int(input("Enter timer Hours : "))
+input_min = int(input("Enter minutes : "))
+input_sec = int(input("Enter second : "))
+
+total = (input_hour * 3600) + (input_min * 60) + input_sec
 os.system('cls' if os.name == 'nt' else 'clear')
-try :
-    while True:
-        print("\033[H", end="")
-        print(" ===== Digital Clock =====\n")
-        digital_time = time.strftime("%H:%M:%S")
-        print("Time :  ",digital_time)
-        second = time.sleep(1)
-except KeyboardInterrupt:
-    print("\nClock off.")
+
+min,sec = divmod(input_sec, 60)
+hrs,min = divmod(min,60)
+time_set = f"{hrs:02d} : {min:02d} : {sec:02d}"
+print(f"You Set Time : {time_set}")
+    
+while total >= 0:
+    print(total)
+    total -= 1
+    time.sleep(1)
+
+    
+    
