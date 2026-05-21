3a0bb7a2317daa9d29026bd66d6661e85724470b
Akash82945 <akashkumar9576386319@gmail.com>
2026-05-22 00:51:19 +0530

    Loading animation with task programs.

--- 11 Python Library/Time Library/5.loading_animation.py
+++ 11 Python Library/Time Library/5.loading_animation.py
@@ -11,14 +11,39 @@
     
     
 
+# import time
+# frame = ['|','/','\\','-']
+# for i in range(20):
+#     spin = frame[i%len(frame)]
+#     print(f"\rLoading {spin}", end="")
+#     time.sleep(0.2)
+    
+    
+    
+
 import time
+import sys
+import threading
 
-frame = ['|','/','\\','-']
+print("We Count number from (1-100000000)")
+def background_task():
+    start = time.time()
+    count = 0
+    for i in range(0,100000000):
+        count += 1
+    end = time.time()
+    print(end - start)
+    # time.sleep(3)
 
-for i in range(20):
+task_thread = threading.Thread(target=background_task)
+task_thread.start()
+
+spinner = ['|','\\','-','/']
+idx = 0
+
+while task_thread.is_alive():
+    print(f"\rWorking... {spinner[idx%len(spinner)]}",end="")
+    idx += 1
+    time.sleep(0.1)
     
-    spin = frame[i%len(frame)]
-    
-    print(f"\rLoading {spin}", end="")
-    time.sleep(2)
-    
+print("\rProcess is Complete")
