b627a71cbe98c01975c3f97486ab5f376de1d958
Akash82945 <akashkumar9576386319@gmail.com>
2026-05-23 22:42:37 +0530

    Delete folder from current or another dictonary.

---
+++ 11 Python Library/5 OS Library/5.delete_folder.py
@@ -0,0 +1,13 @@
+import os
+
+
+# Delete folder from current dictonary
+# folder = os.rmdir('11 Python Library/5 OS Library/Akash Kumar')
+# print("Folder Deleted Successfully.")
+
+
+
+# Delete folder from another dictonary
+targer_folder = r"C:\Users\LENOVO\Desktop\Akash Kumar"
+os.rmdir(targer_folder)
+print("Folder Deleted Successfully.")
