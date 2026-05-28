56b3937e03f4acb0353a176beea5c9b7e9b35907
Akash82945 <akashkumar9576386319@gmail.com>
2026-05-28 23:51:56 +0530

    Logic practice programs.

--- 11 Python Library/9 Requests Library/class_request.py
+++ 11 Python Library/9 Requests Library/class_request.py
@@ -11,34 +11,42 @@ import requests
 # print(response.text)
 
 
-url = "https://api.github.com/users/Akash82945"
+# url = "https://api.github.com/users/Akash82945"
 
-try:
-    response = requests.get(url)
-    response.raise_for_status()
+# try:
+#     response = requests.get(url)
+#     response.raise_for_status()
     
-    data = response.json()
+#     data = response.json()
     
     
-    print('Github profil details')
-    print(f'''
-          Name : {data.get('name')}
-          Bio : {data.get('bio')}
-          Location : {data.get('location')}
-          Repos : {data.get('public_repos')}
-          ''')
+#     print('Github profil details')
+#     print(f'''
+#           Name : {data.get('name')}
+#           Bio : {data.get('bio')}
+#           Location : {data.get('location')}
+#           Repos : {data.get('public_repos')}
+#           ''')
     
-    repo_url = data.get('repos_url')
+#     repo_url = data.get('repos_url')
     
-    repo_response = requests.get(repo_url)
-    repo_response.raise_for_status()
+#     repo_response = requests.get(repo_url)
+#     repo_response.raise_for_status()
     
-    repo_list = repo_response.json()
+#     repo_list = repo_response.json()
     
-    for idx,repo in enumerate(repo_list, start=1):
-        name = repo.get('name')
-        lang = repo.get('language') or 'Unknown'
-        print(f'{idx} : {name} [Language {lang}]')
+#     for idx,repo in enumerate(repo_list, start=1):
+#         name = repo.get('name')
+#         lang = repo.get('language') or 'Unknown'
+#         print(f'{idx} : {name} [Language {lang}]')
     
-except requests.exceptions.RequestException as e:
-    print(f'Error: {e}')
+# except requests.exceptions.RequestException as e:
+#     print(f'Error: {e}')
+
+
+
+
+url = "https://api.github.com/users/Akash82945"
+response = requests.get(url)
+data = response.json()
+print(data)
