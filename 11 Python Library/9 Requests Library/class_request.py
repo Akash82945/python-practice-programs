49be37bffae1ef4dc7b5af6c558787b0f8244089
Akash82945 <akashkumar9576386319@gmail.com>
2026-05-28 23:34:20 +0530

    Get github details.

--- 11 Python Library/9 Requests Library/class_request.py
+++ 11 Python Library/9 Requests Library/class_request.py
@@ -2,5 +2,43 @@ import requests
 
 
 # First internet request
-response = requests.get("https://api.github.com")
-print(response)
+# response = requests.get("https://api.github.com")
+# print(response)
+
+
+
+# response = requests.get("https://api.github.com")
+# print(response.text)
+
+
+url = "https://api.github.com/users/Akash82945"
+
+try:
+    response = requests.get(url)
+    response.raise_for_status()
+    
+    data = response.json()
+    
+    
+    print('Github profil details')
+    print(f'''
+          Name : {data.get('name')}
+          Bio : {data.get('bio')}
+          Location : {data.get('location')}
+          Repos : {data.get('public_repos')}
+          ''')
+    
+    repo_url = data.get('repos_url')
+    
+    repo_response = requests.get(repo_url)
+    repo_response.raise_for_status()
+    
+    repo_list = repo_response.json()
+    
+    for idx,repo in enumerate(repo_list, start=1):
+        name = repo.get('name')
+        lang = repo.get('language') or 'Unknown'
+        print(f'{idx} : {name} [Language {lang}]')
+    
+except requests.exceptions.RequestException as e:
+    print(f'Error: {e}')
