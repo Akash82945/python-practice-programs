90169d58f8a67697c5cde98d2e2167a1e805f295
Akash82945 <akashkumar9576386319@gmail.com>
2026-05-31 23:43:30 +0530

    Build random quotes generator.

---
+++ 11 Python Library/9 Requests Library/4.build_random_quote_featcher.py
@@ -0,0 +1,28 @@
+import requests
+
+
+# Random quotes generator
+
+def get_random_quote():
+
+    url = 'https://dummyjson.com/quotes/random'
+
+    try:
+        
+        response = requests.get(url)
+        response.raise_for_status()
+        
+        data = response.json()
+        
+        quotes = data.get("quote")
+        author = data.get("author")
+        
+        print('=== Random Quotes Generator ===')
+        print(f'\nQuotes : {quotes}')
+        print(f"\nAuthor : {author}")
+        
+    except requests.exceptions.RequestException as e:
+        print(f"Error : {e}")
+        
+
+get_random_quote()
