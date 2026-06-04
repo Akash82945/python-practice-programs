29d2920c29de7a65d5beff629c9243a080cba1fa
Akash82945 <akashkumar9576386319@gmail.com>
2026-06-04 22:56:08 +0530

    Weather app.

---
+++ 11 Python Library/10 API/2.weather_app.py
@@ -0,0 +1,58 @@
+import requests
+from geopy.geocoders import Nominatim     #type: ignore
+
+
+# Weather App
+
+def get_location(city_name):
+    
+    geolocater = Nominatim(user_agent = 'my_geo_app_v1')
+    
+    try:
+        
+        location = geolocater.geocode(city_name)
+        
+        long = location.longitude
+        lat = location.latitude
+        
+        if not city_name:
+            print(f"Error : {city_name} not found.")
+            return
+        
+        url = f'https://wttr.in/{lat},{long}?format=j1'
+        
+        
+        response = requests.get(url)
+        response.raise_for_status()
+        
+        data = response.json()
+        
+        # print(data)
+        
+        current = data['current_condition'][0]
+        
+        # wind_mile = current['windgustmiles']
+        cloudcover = current['cloudcover']
+        humidity = current['humidity']
+        temp_c = current['temp_C']
+        temp_f = current['temp_F']
+        uv_idx = current['uvIndex']
+        observation_time = current['observation_time']
+        
+        
+        print(f'''
+              === Weather App ===
+              City Name : {city}
+              Observation Time : {observation_time}
+              Temperature : {temp_c}'C  &  {temp_f}'F
+              Cloud cover : {cloudcover}
+              Humidity : {humidity}
+              UV Index : {uv_idx}
+              ''')
+        
+    except requests.exceptions.RequestException as e:
+        print(f"Error : {e}")
+        
+        
+city = 'Amritsar'
+get_location(city)
