01be61035d4aef3a0b3ac907a6619042840b132e
Akash82945 <akashkumar9576386319@gmail.com>
2026-06-08 01:32:53 +0530

    Practice advance methods.

--- 12 NumPy Library/first_practical_milestoes.py
+++ 12 NumPy Library/first_practical_milestoes.py
@@ -75,16 +75,49 @@ import numpy as np
 
 
 # 2D array 
-matrix_marks = np.random.randint(1,101,(3,4))
+# matrix_marks = np.random.randint(1,101,(3,4))
 # one condition
 # passed_sub = matrix_marks[matrix_marks>60]
 
 # two condition
-passed_sub = matrix_marks[(matrix_marks>60) & (matrix_marks<80)]
-print(f"\nMatrix Marks : \n{matrix_marks}")
-print(f"\nPassed Subject : \n{passed_sub}")
+# passed_sub = matrix_marks[(matrix_marks>60) & (matrix_marks<80)]
+# print(f"\nMatrix Marks : \n{matrix_marks}")
+# print(f"\nPassed Subject : \n{passed_sub}")
 
-matrix_marks[matrix_marks < 40] = 40
 
-print("\nफेल होने वाले छात्रों को ग्रेस मार्क्स देने के बाद (2D शेप वैसा ही रहेगा):")
-print(matrix_marks)
+
+
+
+# Where method 
+# matrix = np.random.randint(1,101,(3,3))
+# Fill 0 where marks is less then 60
+# passed_sub = np.where(matrix > 60, matrix, 0) 
+# Fill pass or fail text 
+# passed_sub = np.where(matrix > 60, 'Pass', 'Fail')
+# print(f"Matrix : \n{matrix}")
+# print(f"Passed Subject : \n{passed_sub}")
+
+
+
+
+
+# Quick Checking [np.any() | np.all()]
+# 1D array
+# marks = np.random.randint(1,101,5)
+# any_failed = np.any(marks < 40)
+# all_passed = np.all(marks >= 40)
+# print(f"Marks : {marks}")
+# print(f"Any failed [YES or NO] : {any_failed}")
+# print(f"All Passed [YES or NO] : {all_passed}")
+
+# 2D array
+matrix_marks = np.random.randint(1,101,(3,3))
+# any_failed = np.any(matrix_marks < 40)
+any_failed = np.any(matrix_marks < 40, axis=0)   #Check Each row
+# all_passed = np.all(matrix_marks >= 40)
+all_passed = np.all(matrix_marks >= 40, axis=0)  #Check Each row
+print(f'''
+\nMatrix Marks : \n{matrix_marks}
+\nAny failed student : {any_failed}
+\nAll passed student : {all_passed}
+      ''')
