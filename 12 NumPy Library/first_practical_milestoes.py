40b288a5345c4812de1ba9c21e0e1c8939ae6d3e
Akash82945 <akashkumar9576386319@gmail.com>
2026-06-08 23:02:45 +0530

    Add extra functions.

--- 12 NumPy Library/first_practical_milestoes.py
+++ 12 NumPy Library/first_practical_milestoes.py
@@ -111,13 +111,75 @@ import numpy as np
 # print(f"All Passed [YES or NO] : {all_passed}")
 
 # 2D array
-matrix_marks = np.random.randint(1,101,(3,3))
+# matrix_marks = np.random.randint(1,101,(3,3))
 # any_failed = np.any(matrix_marks < 40)
-any_failed = np.any(matrix_marks < 40, axis=0)   #Check Each row
+# any_failed = np.any(matrix_marks < 40, axis=0)   #Check Each row
 # all_passed = np.all(matrix_marks >= 40)
-all_passed = np.all(matrix_marks >= 40, axis=0)  #Check Each row
-print(f'''
-\nMatrix Marks : \n{matrix_marks}
-\nAny failed student : {any_failed}
-\nAll passed student : {all_passed}
-      ''')
+# all_passed = np.all(matrix_marks >= 40, axis=0)  #Check Each row
+# print(f'''
+# \nMatrix Marks : \n{matrix_marks}
+# \nAny failed student : {any_failed}
+# \nAll passed student : {all_passed}
+      # ''')
+      
+      
+      
+      
+      
+      
+# Create 1D array with containing all odd numbers form 1-15
+# arr = np.arange(1,16,2)
+# print(f"Odd Number is : {arr}")
+
+
+# Create 2D matrix with 4row and 5columns filled entirely with the number 1.0
+# matrix = np.ones((4,5))
+# print(f"Matrix : \n{matrix}")
+
+
+# Create 1D array of numbers from 0-11, and then reshape ist into matrix with 3row and 4columns.
+# arr = np.arange(0,12)
+# matrix = arr.reshape(3,4)
+# print(f"1D array : {arr}")
+# print(f"2D array : \n{matrix}")
+
+
+
+
+# Indexing or slicing
+# matrix = np.array([
+#     [10, 11, 12],
+#     [20, 21, 22],
+#     [30, 31, 32]
+# ])
+
+# Extract the number 3,1 from the index
+# spec_index = matrix[2,1]
+# print('Extract no 36 : ',spec_index)
+
+# Extract the entire last row
+# last_row = matrix[2:]
+# print(f"Last Row : {last_row}")
+
+# Extract the sub-grid [[20,21], [30,31]] (the bottom left 2X2 grid)
+# left_grid = matrix[0:3,0:2]
+# print(f"Left 2X2 bottom grid : \{left_grid}")
+
+
+
+
+
+# Data manipulation
+data = np.array([
+    [10, 20, 30],
+    [5,  15, 25],
+    [2,  4,  6]
+])
+
+# Find max value in entire matrix
+max = np.max(data)
+print(f"Max Value : {max}")
+
+# Calculate average value of each column
+avg_each_col = np.mean(data, axis=0)
+print(f"Average val of each col : {avg_each_col}")
