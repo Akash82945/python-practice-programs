74876918073268645f9e5b152407b4778c545aaf
Akash82945 <akashkumar9576386319@gmail.com>
2026-07-02 23:06:33 +0530

    Product wise sales comparision.

--- 14 Matplotlib Library/9.product_sales_comparison.py
+++ 14 Matplotlib Library/9.product_sales_comparison.py
@@ -1,48 +1,20 @@
-# import pandas as pd
-# import matplotlib.pyplot as plt
-
-
-# df = pd.read_csv('Sales.csv')
-# # print(df.columns)
-
-# # df['Final Amount'] =  
-
-# catogery = df['Category']
-# # print(catogery)
-# sales = df['Final Amount'].sum()
-
-# plt.bar(catogery,sales, color='red', edgecolor='black' )
-# plt.title("Product Sales Comparison")
-# plt.xlabel('Product Category')
-# plt.ylabel("Sales Amount(INR)")
-# plt.grid(True, axis='y', linestyle='--')
-# plt.xticks(rotation=35)
-# plt.tight_layout()
-# plt.show()
-
-
-
-
-
-
-# import pandas as pd
-
-# df = pd.read_csv('Sales.csv')
-# print(df.columns)
-
-# df['Final Amount'] = pd.to_numeric(df['Final Amount'],errors='coerce')
-
-# category_sales = df.groupby('Category')['Final Amount'].sum()
-
-# print(category_sales)
-
-
-
-
-
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
+# Show Category wise sale Amount first
+df = pd.read_csv('Sales.csv')
+print(df.columns)
+
+df['Final Amount'] = pd.to_numeric(df['Final Amount'],errors='coerce')
+category_sales = df.groupby('Category')['Final Amount'].sum()
+
+print(category_sales)
+
+
+
+
+# Plot the graph according the tasks
 df = pd.read_csv('Sales.csv')
 
 df['Final Amount'] = pd.to_numeric(df['Final Amount'], errors='coerce')
@@ -50,12 +22,20 @@ df['Final Amount'] = df['Final Amount'].fillna(0)
 
 category_sales = df.groupby('Category')['Final Amount'].sum()
 
-plt.figure(figsize=(10,8))
+fig, ax = plt.subplots(figsize=(9,6))
 
-category_sales.plot(kind='bar', color='firebrick', edgecolor='black', label='Product Sales')
+bar = category_sales.plot(kind='bar', color='firebrick', edgecolor='black', label='Product Sales')
+
+ax.bar_label(ax.containers[0], fmt='₹%.0f', padding=3, fontsize=10)
+
 plt.title('Product Category Wise sale comparison.')
 plt.xlabel('Products Category')
 plt.ylabel('Sales Amount(INR)')
-plt.grid(True, axis='y', linestyle='--')
+
+plt.ylim(0, category_sales.max()*1.15)
+
+plt.grid(True, axis='y', linestyle='--', alpha=0.5)
+plt.xticks(rotation=30)
 plt.legend()
+plt.tight_layout()
 plt.show()
